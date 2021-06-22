a4e5f871a8f461411669483f1e30f81942e4fc5d
louiemc <clouie2@gmail.com>
2021-06-21 20:35:31 -0400

    set the intial dice to be hidden by adding the hidden class to the dice element and adding the hidden class syle in css

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 // Selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
 
+// Set the initial conditions of score0El and score1El
 score0El.textContent = 0;
 score1El.textContent = 0;
+// Add the hidden class to the dice
+diceEl.classList.add("hidden");
