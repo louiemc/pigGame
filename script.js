e629ad649d80f59395ba8c84bde4b9e1c2037980
louiemc <clouie2@gmail.com>
2021-06-26 13:57:17 -0400

    manipulating the dice image in HTML by using the src property on the dice and template literal for the dice png file

--- script.js
+++ script.js
@@ -17,12 +17,11 @@ diceEl.classList.add("hidden");
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
   // 1. Generate a random dice roll
-  const dice = Math.trunc(Math.random() * 6 + 1);
+  const dice = Math.trunc(Math.random() * 6) + 1;
   console.log(dice);
   // this variable here will not be a global variable because each time we roll the dice we want to generate a new number
   // 2. Display the dice
   // remember the starting condition - we hid the dice by adding the hidden class, so we need to remove that
   diceEl.classList.remove("hidden");
-
-  // 3. Check for rolled 1: if true, switch to next player
+  diceEl.src = `dice-${dice}.png`;
 });
