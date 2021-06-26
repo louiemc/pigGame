ba89d61da930c97faf34d205b7638fe50cfc868b
louiemc <clouie2@gmail.com>
2021-06-26 14:24:35 -0400

    added current score elements to manipulate, displayed the current score for player 1 (0) for now

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -25,8 +28,14 @@ btnRoll.addEventListener("click", function () {
   diceEl.classList.remove("hidden");
   diceEl.src = `dice-${dice}.png`;
 
+  let currentScore = 0;
+
   // 3. Check for rolled 1
   if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    // Display the score
+    current0El.textContent = currentScore; // CHANGE LATER
   } else {
     // switch to next player
   }
