faf77b00a349daba34044acf4b1519d3f9251711
louiemc <clouie2@gmail.com>
2021-06-26 14:29:35 -0400

    fixed the currentScore variable and put it outside of the btnRoll event handler function, fixed the relative path to the dice pngs

--- script.js
+++ script.js
@@ -16,19 +16,18 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 // Add the hidden class to the dice
 diceEl.classList.add("hidden");
+let currentScore = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
   // 1. Generate a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  const dice = Math.trunc(Math.random() * 6 + 1);
   console.log(dice);
   // this variable here will not be a global variable because each time we roll the dice we want to generate a new number
   // 2. Display the dice
   // remember the starting condition - we hid the dice by adding the hidden class, so we need to remove that
   diceEl.classList.remove("hidden");
-  diceEl.src = `dice-${dice}.png`;
-
-  let currentScore = 0;
+  diceEl.src = `./images/dice-${dice}.png`;
 
   // 3. Check for rolled 1
   if (dice !== 1) {
